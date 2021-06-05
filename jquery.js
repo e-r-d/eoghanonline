5add7d82163ac69bf14ea4772abc2aa1fbb54ad0
erd <eoghanrdempsey@gmail.com>
2021-06-04 22:55:03 -0400

    friday bunch of stuff jquery loader etc

---
+++ jquery.js
@@ -0,0 +1,49 @@
+//highlights category in NAV bar on hover//
+//art and design//
+$(function() {
+  $('.alife3dmagnet, .world, .globeteeshots, .trueblue, .rock, .bunb, .mcnike, .stayinside, .tobaccointernet, .wewontgohome, .tobaccointernetexhibit, .eoghanalife, .lightlink').hover(function() {
+    $('#andlink').css('color', 'blue');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#andlink').css('color', '');
+  });
+});
+
+
+//Photography//
+$(function() {
+  $('.deerlightleak, .nuns, .karshe, .vhs, .tobaccointernet, .wewontgohome, .tobaccointernetexhibit, .eoghanalife, .lightlink').hover(function() {
+    $('#photolink').css('color', 'blue');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#photolink').css('color', '');
+  });
+});
+
+
+//Personal//
+$(function() {
+  $('.passport, .dempseydewalt, .email, .eoghanalife').hover(function() {
+    $('#homelink').css('color', 'blue');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('#homelink').css('color', '');
+  });
+});
+
+
+
+/*
+$(function() {
+$('.eoghanalife').hover(function(){
+  $('.eoghanalife').show();
+});
+});
+*/
+
+
+$(function(){
+  $(".people").mouseover(function(){
+    $(this).toggle();
+  });
+});
